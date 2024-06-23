e062688bfe414fb0f6cd307f6a9d8f90968f5561
sandile323 <sandilehc@gmail.com>
2024-06-23 12:54:00 +0200

    :sparkles: update coolection

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,19 +1,19 @@
 import fs from "fs";
 import path from "path";
-import SectionCollection, { sectionCollectionProps } from "../components/section";
+import SectionCollection, {
+  sectionCollectionProps,
+} from "../components/section";
 import { FunctionComponent } from "react";
 
 interface PageProps {
   params: { slug: string };
-  sectionCollection: sectionCollectionProps;
 }
 
-const Page: FunctionComponent<PageProps> = ({ params, sectionCollection }) => {
+const Page: FunctionComponent<PageProps> = ({ params }) => {
   const slug = params.slug;
 
   const filePath = path.resolve(process.cwd(), "pages.json");
   const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-  
 
   const pageData = jsonData.pages.find(
     (page: any) => page.slug.replace("/", "") === slug
@@ -29,7 +29,7 @@ const Page: FunctionComponent<PageProps> = ({ params, sectionCollection }) => {
     //   <p>My Page: {pageData.slug}</p>
     // </div>
     <>
-    <SectionCollection sections={pageData.sections}/>
+      <SectionCollection sections={pageData.sections} />
     </>
   );
 };
