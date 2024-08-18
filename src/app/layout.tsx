329aea77d2693fae40353042add1523bcdfd488d
sandile323 <sandilehc@gmail.com>
2024-08-18 20:32:04 +0200

    :sparkles: gtm tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,28 @@ export default function RootLayout({
   const sitePaths = GetSitePaths();
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MT4WMJ53');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${inter.className} font-lora`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MT4WMJ53"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header sitePaths={sitePaths} />
         {children}
         <Footer {...footerData} />
